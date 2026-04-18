4495aeffea204e413cd497aaa2ce29a080106bc6
joao <joaopedroazevedo1980@gmail.com>
2026-04-17 22:40:17 -0300

    Soma2Numeros

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Calcular numero");
+int primeironumero, segundonumero, soma;
+
+Console.Write("Digite o primeiro número:");
+primeironumero = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Digite o segundo número:");
+segundonumero = Convert.ToInt32(Console.ReadLine());
+
+soma = primeironumero + segundonumero;
+Console.WriteLine($"soma: {soma}");
